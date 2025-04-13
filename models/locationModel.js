95afd3965549a8176a8822471e62864ac89e647e
Christiaan.K <80765331+Justchrisk@users.noreply.github.com>
2025-04-13 23:01:26 +0200

    Error handling
    Error handling

--- models/locationModel.js
+++ models/locationModel.js
@@ -3,11 +3,19 @@ import config from "../assets/data/config.js";
 export default class LocationModel {
     
     static getCity() {
-        const city = localStorage.getItem(config.LOCAL_STORAGE_KEY);
-        return city ? city : config.DEFAULT_CITY;
+        try {
+            const city = localStorage.getItem(config.LOCAL_STORAGE_KEY);
+            return city ? city : config.DEFAULT_CITY;
+        } catch (e) {
+            console.warn('Could not access localStorage, Default city will be used instead:', e);
+            return config.DEFAULT_CITY;
+        }
     }
 
     static setCity(city) {
-        localStorage.setItem(config.LOCAL_STORAGE_KEY, city);
-    }
+        try {
+            localStorage.setItem(config.LOCAL_STORAGE_KEY, city);
+        } catch (e) {
+            console.warn('Unable to set selected city in localStorage:', e);
+        }    }
 }
